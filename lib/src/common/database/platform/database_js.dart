76574ef58a4f3d7d808d6e553ed9ffc120ef02db
Plague Fox <plugfox@gmail.com>
2024-04-06 23:33:53 +0400

    Refactor database initialization and add support for memory database

--- lib/src/common/database/platform/database_js.dart
+++ lib/src/common/database/platform/database_js.dart
@@ -1,5 +1,7 @@
 // ignore_for_file: avoid_web_libraries_in_flutter
 
+import 'dart:html' as html;
+
 import 'package:drift/drift.dart';
 import 'package:drift/wasm.dart' as wasm;
 import 'package:invoice/src/common/constant/config.dart';
@@ -18,21 +20,20 @@ Future<QueryExecutor> $createQueryExecutor({
   bool memoryDatabase = false,
 }) async {
   // https://drift.simonbinder.eu/web
+  /*
+    if (memoryDatabase) {
+      final sqlite3 = await WasmSqlite3.loadFromUrl(Uri.parse('sqlite3.wasm'));
+      sqlite3.registerVirtualFileSystem(InMemoryFileSystem(), makeDefault: true);
+      WasmDatabase.inMemory(sqlite3);
+    }
+  */
+  if (dropDatabase) html.window.indexedDB!.deleteDatabase(Config.databaseName);
   final result = await wasm.WasmDatabase.open(
     databaseName: memoryDatabase ? ':memory:' : path ?? Config.databaseName,
     sqlite3Uri: Uri.parse('sqlite3.wasm'),
     driftWorkerUri: Uri.parse('drift_worker.js'),
   );
 
-  if (result.missingFeatures.isNotEmpty) {
-    // Depending how central local persistence is to your app, you may want
-    // to show a warning to the user if only unrealiable implemetentations
-    // are available.
-    l.w('Using ${result.chosenImplementation} due to missing browser '
-        'features: ${result.missingFeatures}');
-  }
-
-  return result.resolvedExecutor;
   /*
     if (dropDatabase) html.window.localStorage.clear();
     return Future<QueryExecutor>.value(
@@ -43,4 +44,14 @@ Future<QueryExecutor> $createQueryExecutor({
       ),
     );
   */
+
+  if (result.missingFeatures.isNotEmpty) {
+    // Depending how central local persistence is to your app, you may want
+    // to show a warning to the user if only unrealiable implemetentations
+    // are available.
+    l.w('Using ${result.chosenImplementation} due to missing browser '
+        'features: ${result.missingFeatures}');
+  }
+
+  return result.resolvedExecutor;
 }
